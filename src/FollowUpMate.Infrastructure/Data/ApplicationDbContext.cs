b7dbd7e216669f0e8fd45b41a506e70b9540b4b4
Muhammad Ahmad <muhammadahmad399@gmail.com>
2025-08-06 20:19:22 +0500

    seed roles data and added common classes

--- src/FollowUpMate.Infrastructure/Data/ApplicationDbContext.cs
+++ src/FollowUpMate.Infrastructure/Data/ApplicationDbContext.cs
@@ -28,6 +28,36 @@ namespace FollowUpMate.Infrastructure.Data
             builder.Entity<IdentityUserLogin<Guid>>().ToTable("UserLogins");
             builder.Entity<IdentityRoleClaim<Guid>>().ToTable("RoleClaims");
             builder.Entity<IdentityUserToken<Guid>>().ToTable("UserTokens");
+
+            SeedIdentityRoles(builder);
+        }
+
+        private void SeedIdentityRoles(ModelBuilder builder)
+        {
+            builder.Entity<IdentityRole<Guid>>().HasData(
+                new IdentityRole<Guid>
+                {
+
+                    Id = Guid.Parse("4629D1F6-D91E-4E92-9B65-00CD28A10A06"),
+                    Name = "Admin",
+                    NormalizedName = "ADMIN",
+                    ConcurrencyStamp = "59F0CD01-70B6-4DD5-A6D6-A41B8CA15BCA"
+                },
+                new IdentityRole<Guid>
+                {
+                    Id = Guid.Parse("98EDBC68-DE0A-47DC-A94F-3D3B76E9FC99"),
+                    Name = "Sales",
+                    NormalizedName = "SALES",
+                    ConcurrencyStamp = "990C9461-B922-419A-AB90-D6223781A3D0"
+                },
+                new IdentityRole<Guid>
+                {
+                    Id = Guid.Parse("F38700B2-937B-4155-8819-738291E02DB5"),
+                    Name = "User",
+                    NormalizedName = "USER",
+                    ConcurrencyStamp = "73356855-2611-4F6C-8E11-6D4589D80153"
+                }
+            );
         }
     }
 }
--- src/FollowUpMate.Infrastructure/Migrations/20250806151757_seed_roles.Designer.cs
+++ src/FollowUpMate.Infrastructure/Migrations/20250806151757_seed_roles.Designer.cs
@@ -4,6 +4,7 @@ using FollowUpMate.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FollowUpMate.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250806151757_seed_roles")]
+    partial class seed_roles
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
